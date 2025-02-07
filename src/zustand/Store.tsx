d497ce8aac6c7b4e7812ec5a9f42d81f49777e76
uni <147740687+shunmakk@users.noreply.github.com>
2025-02-07 22:44:29 +0900

    logger

--- src/zustand/Store.tsx
+++ src/zustand/Store.tsx
@@ -1,4 +1,5 @@
-import { create } from "zustand";
+/* eslint-disable @typescript-eslint/no-explicit-any */
+import { create, StateCreator } from "zustand";
 
 //型の定義
 type StoreStateType = {
@@ -7,19 +8,36 @@ type StoreStateType = {
   showhappys: () => void;
 };
 
+// logger、状態を更新するたびにその変更をコンソールに記録する
+const logger =
+  (config: StateCreator<StoreStateType>) => (set: any, get: any, api: any) =>
+    config(
+      (args) => {
+        console.log("前の状態:", get().happys);
+        set(args);
+        //argの中身を取得(どのような処理が行われているか確認できる)
+        console.log(args);
+        console.log("次の状態:", get().happys);
+      },
+      get,
+      api
+    );
+
 //storeの作成
-const useStore = create<StoreStateType>()((set) => ({
-  //初期値
-  happys: 0,
-  //状態の更新（set関数を使用）
-  happysUp: () =>
-    set((state) => ({
-      happys: state.happys + 1,
-    })),
-  //状態の読み取り
-  showhappys: () => {
-    alert(`Current happys:${useStore.getState().happys}`);
-  },
-}));
+const useStore = create<StoreStateType>()(
+  logger((set, get) => ({
+    //初期値
+    happys: 0,
+    //状態の更新（set関数を使用）
+    happysUp: () =>
+      set((state) => ({
+        happys: state.happys + 1,
+      })),
+    //状態の読み取り
+    showhappys: () => {
+      alert(`Current happys:${get().happys}`);
+    },
+  }))
+);
 
 export { useStore };
