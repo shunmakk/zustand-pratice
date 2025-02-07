0781a67f25e9452b89d0b5e40afc23e4909fa41e
uni <147740687+shunmakk@users.noreply.github.com>
2025-02-08 01:42:48 +0900

    immer

--- src/zustand/Store.tsx
+++ src/zustand/Store.tsx
@@ -1,5 +1,5 @@
-/* eslint-disable @typescript-eslint/no-explicit-any */
-import { create, StateCreator } from "zustand";
+import { create } from "zustand";
+import { immer } from "zustand/middleware/immer";
 
 //型の定義
 type StoreStateType = {
@@ -9,30 +9,31 @@ type StoreStateType = {
 };
 
 // logger、状態を更新するたびにその変更をコンソールに記録する
-const logger =
-  (config: StateCreator<StoreStateType>) => (set: any, get: any, api: any) =>
-    config(
-      (args) => {
-        console.log("前の状態:", get().happys);
-        set(args);
-        //argの中身を取得(どのような処理が行われているか確認できる)
-        console.log(args);
-        console.log("次の状態:", get().happys);
-      },
-      get,
-      api
-    );
+// const logger =
+//   (config: StateCreator<StoreStateType>) => (set: any, get: any, api: any) =>
+//     config(
+//       (args) => {
+//         console.log("前の状態:", get().happys);
+//         set(args);
+//         //argの中身を取得(どのような処理が行われているか確認できる)
+//         console.log(args);
+//         console.log("次の状態:", get().happys);
+//       },
+//       get,
+//       api
+//     );
 
 //storeの作成
 const useStore = create<StoreStateType>()(
-  logger((set, get) => ({
+  immer((set, get) => ({
     //初期値
     happys: 0,
     //状態の更新（set関数を使用）
     happysUp: () =>
-      set((state) => ({
-        happys: state.happys + 1,
-      })),
+      set((state) => {
+        //immerを使うとコードがより直感的にかける
+        state.happys++;
+      }),
     //状態の読み取り
     showhappys: () => {
       alert(`Current happys:${get().happys}`);
